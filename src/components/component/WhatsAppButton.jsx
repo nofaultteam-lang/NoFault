834517c1b81738267e40cfa29048246a5a3d0cb4
nofaultteam-lang <nofaultteam@gmail.com>
2025-09-08 23:35:49 +0530

    Update WhatsAppButton.jsx

--- src/components/component/WhatsAppButton.jsx
+++ src/components/component/WhatsAppButton.jsx
@@ -4,7 +4,7 @@ import { HelpCircle } from "lucide-react"; // Lucide help icon
 const HelpButton = () => {
   return (
     <a
-      href="https://wa.me/919999999999" // Replace with your number or help link
+      href="https://wa.me/917409275180" // Replace with your number or help link
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-blue-600 hover:bg-blue-700 text-white p-4 rounded-full shadow-lg flex items-center justify-center z-50 transition-all"
